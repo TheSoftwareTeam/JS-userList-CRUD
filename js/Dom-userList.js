08f7041b5c0f3949d9ed2dc620108053540afc6b
mahirkursun <mahirkrsn@gmail.com>
2023-07-28 21:14:16 +0300

    master

--- js/Dom-userList.js
+++ js/Dom-userList.js
@@ -23,7 +23,11 @@ function control() {
 
   if (firstname != "" && lastname != "") {
     for (let i = 0; i < users.length; i++) {
-      if (users[i].ad == firstname && users[i].soyad == lastname && users[i].job==job) {
+      if (
+        users[i].ad == firstname &&
+        users[i].soyad == lastname &&
+        users[i].job == job
+      ) {
         cleanInput();
         kulVarmi = true;
       }
@@ -44,7 +48,12 @@ function AddUser() {
       window.alert("Bu kullanici zaten var.");
       cleanInput();
     } else {
-      users.push({ id: users.length, ad: firstname, soyad: lastname, job: job });
+      users.push({
+        id: users.length,
+        ad: firstname,
+        soyad: lastname,
+        job: job,
+      });
       window.alert("Kullanıcı eklendi.");
       cleanInput();
     }
@@ -54,18 +63,16 @@ function AddUser() {
   userShow();
 }
 
-function userShow()
- {
+function userShow() {
   document.getElementById("userList").innerHTML = "";
 
-  for (let name of users) {
+  for (let name in users) {
     let kullaniciDivi = document.createElement("div");
 
     kullaniciDivi.classList = "userCard";
-
-    kullaniciDivi.setAttribute("id", name.id);
-    kullaniciDivi.setAttribute("onClick", `getUser(${name.id})`);
-
+    
+    kullaniciDivi.setAttribute("id", name);
+    
     document.getElementById("userList").appendChild(kullaniciDivi);
 
     let textH2 = document.createElement("h2");
@@ -73,30 +80,36 @@ function userShow()
     textH2.setAttribute("id", "isimh2");
     kullaniciDivi.appendChild(textH2);
 
-    textH2.innerHTML = name.ad + " " + name.soyad;
-    kullaniciDivi.innerHTML += "<h5>" + name.job + "</h5>";
+    textH2.innerHTML = users[name].ad + " " + users[name].soyad;
+    kullaniciDivi.innerHTML += "<h5>" + users[name].job + "</h5>";
 
-    let btnSil=document.createElement("a");
-    btnSil.classList="btnDelete";
-    btnSil.setAttribute("id",name.id);
-    btnSil.setAttribute("onClick", `userDelete(${name.id})`);
+    let btnSil = document.createElement("a");
+    btnSil.classList = "btnDelete";
+    btnSil.setAttribute("id", name);
+    btnSil.setAttribute("onClick", `userDelete(${name})`);
     kullaniciDivi.appendChild(btnSil);
-    btnSil.innerHTML="sil";
+    btnSil.innerHTML = "sil";
 
+
+    let btnUpdate = document.createElement("a");
+    btnUpdate.classList = "btnUpdate";
+    btnUpdate.setAttribute("id", name);
+    btnUpdate.setAttribute("onClick", `getUser(${name})`);
+    kullaniciDivi.appendChild(btnUpdate);
+    btnUpdate.innerHTML = "güncelle";
+
+
+  }
 }
- }
 
-
-
-function userDelete(id)
-{
-  
-    users.splice(id,1);
-    console.log(users);
-    userShow();
-    cleanInput();
-  
- 
+function userDelete(id) {
+  if (users.length == 1) {
+    users.splice(0, 1);
+  }
+  users.splice(id, 1);
+  console.log(users);
+  userShow();
+  cleanInput();
 }
 
 function cleanInput() {
